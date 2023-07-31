d375c214070ff948c317f765ea13b4f527e6eddf
Hillel331 <140709384+Hillel331@users.noreply.github.com>
2023-07-31 19:35:31 +0300

    Update symbol_dict.c

--- symbol_dict.c
+++ symbol_dict.c
@@ -1,14 +1,18 @@
 #include "symbol_dict.h"
 
-void insert(symbol_dict *symbols, char *name, int address, boolean is_data, boolean is_entry)
+void insert(symbol_dict *symbols, char *name, int address, int line, boolean is_extern, boolean is_entry)
 {
-    int index = tolower(name[0]) - 'a';
-    add_symbol(&(symbols->dict[index]), name, address, is_data, is_entry);
+    int index;
+    index = tolower(name[0]) - 'a';
+    add_symbol(&(symbols->dict[index]), name, address,line, is_extern, is_entry);
 }
 
 symbol_node *find(symbol_dict *symbols, char *name)
 {
-    int index = tolower(name[0]) - 'a';   
+    int index;
+    if(!isalpha(*name))
+        return NULL;
+    index = tolower(name[0]) - 'a';   
     return find_symbol(symbols->dict[index], name);
 }
 
