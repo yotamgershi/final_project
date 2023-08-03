6560b6ccf3020611cb3dad729e3e52c5c052eb93
Hillel331 <140709384+Hillel331@users.noreply.github.com>
2023-08-03 15:56:39 +0300

    Update first_pass.h

--- first_pass.h
+++ first_pass.h
@@ -5,7 +5,17 @@
 #include "pre_assembler.h"
 #include "errors.h"
 
-symbol_dict *first_pass(pre_assembled_file); /* TODO: change FILE *first_pass_file to dict *symbol_table */
-boolean validate_line(char *line);
+
+symbol_dict *first_pass(FILE * pre_assembled_file,int *DC,int *IC,int *error_first_pass);
+int validate_line(char *line, int line_number);
+
+int is_extern(char * line);
+int is_entry(char * line);
+void get_label_name(char * line,char * name,int internal);
+void is_repeat_def(symbol_node *current_nude,int line_number,int externl,int internal,int *error_first_pass,int DC, int IC, int dc);
+int is_label_line(char *line);
+void insert_extern(symbol_node *symbol_table,char *line,int line_number,int *error_first_pass);
+int count_data(char *line);
+int count_instructions(char *line);
 
 #endif
