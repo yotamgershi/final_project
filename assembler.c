c25f6b87fe68f009c5d8489963429284cb6c0f30
Hillel331 <140709384+Hillel331@users.noreply.github.com>
2023-08-03 15:58:32 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -6,18 +6,22 @@ int main (int argc, char *argv[]) {
         printf("Usage: ./assembler file1 file2 ...");
     for (i = 1; i < argc; i += 1)
     {
+        int DC=0,IC=0;
+        int error_first_pass=false;
+        symbol_dict *symbol_table; 
         FILE *pre_assembled_file = pre_assembler(argv[i]);
         if (pre_assembled_file)
         {
-            /*FILE *first_pass_file = first_pass(pre_assembled_file); /* TODO: change FILE *first_pass_file to dict *symbol_table 
-                if (first_pass_file)
+                symbol_table = first_pass(pre_assembled_file,&DC,&IC,&error_first_pass); ] 
+                if (!error_first_pass) /* No error in first pass move to second */
                 {
-                    FILE *assembled_file = second_pass(first_pass_file); /* change FILE *assembled_file to dict *DATA_IMAGE and dict *CODE_IMAGE 
+                 /* FILE *assembled_file = second_pass(first_pass_file); /* change FILE *assembled_file to dict *DATA_IMAGE and dict *CODE_IMAGE 
                     if (assembled_file)
                     {
                         output(assembled_file);
-                    }
-                }*/
+                    }*/
+                }
+               /* NEED TO FREE symbol_table !!!! */
         }
     }
     
