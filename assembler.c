b808357705ef3e2fc1fdc1026efca96f752162f2
Hillel331 <140709384+Hillel331@users.noreply.github.com>
2023-08-08 14:26:03 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -1,5 +1,7 @@
 #include "assembler.h"
 
+
+
 int main (int argc, char *argv[]) {
     int i;
     if (argc == 1)
@@ -10,18 +12,13 @@ int main (int argc, char *argv[]) {
         int error_first_pass=false;
         symbol_dict *symbol_table; 
         FILE *pre_assembled_file = pre_assembler(argv[i]);
+
         if (pre_assembled_file)
         {
-                symbol_table = first_pass(pre_assembled_file,&DC,&IC,&error_first_pass); ] 
-                if (!error_first_pass) /* No error in first pass move to second */
-                {
-                 /* FILE *assembled_file = second_pass(first_pass_file); /* change FILE *assembled_file to dict *DATA_IMAGE and dict *CODE_IMAGE 
-                    if (assembled_file)
-                    {
-                        output(assembled_file);
-                    }*/
-                }
-               /* NEED TO FREE symbol_table !!!! */
+            symbol_table = first_pass(pre_assembled_file,&DC,&IC,&error_first_pass); 
+            if (!error_first_pass) /* No error in first pass move to second */ 
+                second_pass(argv[i],DC,IC,symbol_table);
+            free_dictionary_symbol(symbol_table);     
         }
     }
     
