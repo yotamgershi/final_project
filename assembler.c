ce926ba16a23cf71275f750b642ed07fa9b37100
Hillel331 <140709384+Hillel331@users.noreply.github.com>
2023-08-12 23:02:49 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -3,22 +3,31 @@
 
 
 int main (int argc, char *argv[]) {
+
     int i;
+    int DC,IC;
+    int error_first_pass;
+    symbol_dict *symbol_table;
+    FILE *pre_assembled_file;
+
     if (argc == 1)
         printf("Usage: ./assembler file1 file2 ...");
-    for (i = 1; i < argc; i += 1)
-    {
-        FILE *pre_assembled_file = pre_assembler(argv[i]);
 
-        if (pre_assembled_file)
+    for (i = 1; i < argc; i++)
+    {
+        DC=0;
+        IC=0;
+        error_first_pass=false;
+ 
+        pre_assembled_file = pre_assembler(argv[i]);
+        if (pre_assembled_file) /* No error in pre_assembled move to first pass */
         {
             symbol_table = first_pass(pre_assembled_file,&DC,&IC,&error_first_pass); 
             if (!error_first_pass) /* No error in first pass move to second */ 
                 second_pass(argv[i],DC,IC,symbol_table);
             free_dictionary_symbol(symbol_table);     
         }
-    }
-    
+    } 
     return 0;
 }
 
